5e7b979f3912b8ea37826f0166da9a3b0d2bfd02
MrSimbax <mateuszprzybyl@simbax.info>
2016-12-01 18:38:34 +0100

    Create Maven project

---
+++ go-root/go-client/src/main/java/pwrrgmp2017/go/client/App.java
@@ -0,0 +1,13 @@
+package pwrrgmp2017.go.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ go-root/go-server/src/main/java/pwrrgmp2017/go/server/App.java
@@ -0,0 +1,13 @@
+package pwrrgmp2017.go.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

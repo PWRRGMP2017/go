f10bc4308df2218cb8e23fd2e4e02ed00666e308
MrSimbax <mateuszprzybyl@simbax.info>
2016-12-08 23:19:35 +0100

    Back to console client

--- go-root/go-client/src/main/java/pwrrgmp2017/go/client/ClientMain.java
+++ go-root/go-client/src/main/java/pwrrgmp2017/go/client/ClientMain.java
@@ -1,20 +1,18 @@
 package pwrrgmp2017.go.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 /**
  * Runs the client
  */
-public class ClientMain extends Application
+public class ClientMain
 {
 	private static final Logger LOGGER = Logger.getLogger(ClientMain.class.getName());
 
@@ -34,51 +32,59 @@ public class ClientMain extends Application
 		}
 		catch (NumberFormatException e)
 		{
-			LOGGER.severe("Port must be a number: " + args[1]);
+			LOGGER.log(Level.SEVERE, "Port must be a number: {0}", args[1]);
 			return;
 		}
 
 		if (port < 5001)
 		{
-			LOGGER.severe("Port must be greater than 5000: " + port);
+			LOGGER.log(Level.SEVERE, "Post must be greater than 5000: {0}", port);
 			return;
 		}
 
+		Socket socket;
+		BufferedReader input;
+		PrintWriter output;
+
+		LOGGER.log(Level.INFO, "Client is starting.");
 		try
 		{
-			new ServerConnection(hostname, port);
+			socket = new Socket(hostname, port);
+			LOGGER.log(Level.INFO, "Client is running.");
+
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream(), true);
+
+			System.out.println("Server says: " + input.readLine());
 		}
 		catch (IOException e)
 		{
-			LOGGER.severe("Could not connect to server: " + e.getMessage());
+			LOGGER.log(Level.SEVERE, "Could not connect to server: " + e.getMessage());
 			return;
 		}
 
-		launch(args);
+		Scanner scanner = new Scanner(System.in);
+		String message;
+		while (true)
+		{
+			try
+			{
+				System.out.print("Wpisz coś: ");
+				message = scanner.nextLine();
+				output.println(message);
+				System.out.println(input.readLine());
+			}
+			catch (IOException e)
+			{
+				LOGGER.log(Level.WARNING, "Problem with server connection: " + e.getMessage());
+				return;
+				// Continue
+			}
+		}
 	}
 
 	private static void printUsage()
 	{
 		System.out.println("usage: java go-client.jar hostname port");
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
-		primaryStage.setTitle("Hello World!");
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>()
-		{
-			public void handle(ActionEvent event)
-			{
-				System.out.println("Hello World!");
-			}
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250));
-		primaryStage.show();
 	}
 }

8963598ca29c524513e1d0e79ac86a7984bd17ff
Robert Gawlik <robert.gawlik1996@gmail.com>
2016-12-08 02:44:25 +0100

    Change the types of Collections for correct use in GamesManager

--- go-root/go-server/src/main/java/pwrrgmp2017/go/server/GamesManager.java
+++ go-root/go-server/src/main/java/pwrrgmp2017/go/server/GamesManager.java
@@ -1,23 +1,26 @@
 package pwrrgmp2017.go.server;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import pwrrgmp2017.go.server.Exceptions.LostPlayerConnection;
 
 public class GamesManager
 {
 	private List<Game> games;
-	private List<PlayerConnection> chosingPlayers;
+	private ConcurrentHashMap<String, PlayerConnection> chosingPlayers;
 	private List<PlayerConnection> playingPlayers;
-	private List<PlayerConnection> waitingPlayers;
+	private LinkedList<PlayerConnection> waitingPlayers;
 
 	GamesManager()
 	{
 		games = new ArrayList<Game>();
-		chosingPlayers = new ArrayList<PlayerConnection>();
+		chosingPlayers = new ConcurrentHashMap<String, PlayerConnection>();
 		playingPlayers = new ArrayList<PlayerConnection>();
-		waitingPlayers = new ArrayList<PlayerConnection>();
+		//waitingPlayers = (LinkedHashMap<String, PlayerConnection>) Collections.synchronizedMap(new LinkedHashMap<String,PlayerConnection>());
+		waitingPlayers= new LinkedList<PlayerConnection>();
 	}
 
 	public void createGame(PlayerConnection player, PlayerConnection opponent, Object typeGame) // enum?

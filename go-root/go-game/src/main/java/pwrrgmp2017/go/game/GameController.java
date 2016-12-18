5acc858da78a083cb54a5fcefbf00ac06cca2e4c
Robert Gawlik <robert.gawlik1996@gmail.com>
2016-12-18 04:22:04 +0100

    Improvement GameController by add basic methods

--- go-root/go-game/src/main/java/pwrrgmp2017/go/game/GameController.java
+++ go-root/go-game/src/main/java/pwrrgmp2017/go/game/GameController.java
@@ -1,5 +1,8 @@
 package pwrrgmp2017.go.game;
 
+import pwrrgmp2017.go.game.Exceptions.BadFieldException;
+import pwrrgmp2017.go.game.GameStates.GameStateEnum;
+import pwrrgmp2017.go.game.Model.GameBoard.Field;
 import pwrrgmp2017.go.game.Model.GameModel;
 
 public class GameController
@@ -11,18 +14,43 @@ public class GameController
 		this.model = model;
 	}
 
-	public boolean[][] getPossibleMovements(String colour)
+	public boolean[][] getPossibleMovements(Field colour) throws BadFieldException
 	{
 		return model.getPossibleMovements(colour);
 	}
 
-	String addMovement(String move)
+	boolean addMovement(int x, int y, Field playerField)
 	{
-		return model.addMovement(move);
+		return model.addMovement(x, y, playerField);
 	}
 
-	boolean isTurnPossible(int x, int y)
+	boolean isTurnPossible(int x, int y, Field playerField) throws BadFieldException
 	{
-		return model.isTurnPossible(x, y);
+		return model.isTurnPossible(x, y, playerField);
+	}
+	
+	public float calculateScore()
+	{
+		return model.calculateScore();
+	}
+	
+	public Field[][] getPossibleTerritory()
+	{
+		return model.getPossibleTerritory();
+	}
+	
+	public GameStateEnum getState()
+	{
+		return model.getState();
+	}
+	
+	public Field[][] getBoardCopy()
+	{
+		return model.getBoardCopy();
+	}
+	
+	public float getKomi()
+	{
+		return model.getKomi();
 	}
 }

c0100bb2deddb857f9330f6388ac5823b00ad569
MrSimbax <mateuszprzybyl@simbax.info>
2017-02-01 19:03:04 +0100

    Fix timeout exceptions

--- go-root/go-webapp/app/models/BotPlayer.java
+++ go-root/go-webapp/app/models/BotPlayer.java
@@ -1,16 +1,27 @@
 package models;
 
 import akka.actor.UntypedActor;
+import models.msgs.GetPlayerName;
 
 public class BotPlayer extends UntypedActor
 {
 	public BotPlayer()
 	{
 	}
+	
 	@Override
 	public void onReceive(Object message) throws Exception
 	{
+		if (message instanceof GetPlayerName)
+		{
+			onGetPlayerName((GetPlayerName) message);
+		}
 		return;
+	}
+	
+	private void onGetPlayerName(GetPlayerName message) 
+	{ 
+		getSender().tell("Bot", getSelf()); 
 	}
 
 }
